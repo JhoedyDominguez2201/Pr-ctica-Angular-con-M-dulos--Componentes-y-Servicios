590a0bb415965de683d0dde0a2b74ed2c8142806
Jhoedy Indurain Dominguez Dominguez <l1739033@chetumal.tecnm.mx>
2021-05-28 08:27:37 -0500

    Commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
